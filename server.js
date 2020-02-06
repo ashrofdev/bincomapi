53b990038bcdc77e192fcd05570b592a97c11749
First Last <salmanashraf853@gmail.com>
2020-02-06 12:42:12 +0100

    deploying

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const knex = require('knex')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true
-    }
-})
 // const db = knex({
 //     client: 'pg',
 //     connection: {
-//       host: '127.0.0.1',
-//       database: 'test'
+//       connectionString : process.env.DATABASE_URL,
+//       ssl: true
 //     }
 // })
+const db = knex({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'test'
+    }
+})
 
 const app = express()
 app.use(bodyParser.json())
@@ -58,29 +58,45 @@ app.get('/results', (req, res)=>{
 //     })
 // })
 
+
+
 app.get('/pdp', (req, res)=>{
-    const units = [
-        {
-            party_abbreviation: 'PDP',
-            scores: []
-        },
-        {
-            party_abbreviation: 'ACN',
-            scores: []
-        }
-    ]
+    const units = {
+        PDP: [],
+        DPP: [],
+        ACN: [],
+        PPA: [],
+        CDC: [],
+        JP: [],
+        ANPP: [],
+        LABOUR: [],
+        CPP: [],
+    }
     db.select('*').from('announced_pu_results').then(data=>{
         data.forEach(e=>{
-            
-            units.forEach(unit=>{
-                if (e.party_abbreviation === unit.party_abbreviation) {
-                    unit.scores.push(parseInt(e.party_score))
-                    console.log(e.party_score)
-                }
-            })
+    
+            if (e.party_abbreviation === 'PDP') {
+                unit.PDP.push(parseInt(e.party_score))
+            } else if (e.party_abbreviation === 'DPP') {
+                unit.DPP.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'ACN') {
+                unit.ACN.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'PPA') {
+                unit.PPA.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'CDC') {
+                unit.CDC.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'JP') {
+                unit.JP.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'ANPP') {
+                unit.ANPP.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'LABOUR') {
+                unit.LABOUR.push(parseInt(e.party_score))
+            }else if (e.party_abbreviation === 'CPP') {
+                unit.CPP.push(parseInt(e.party_score))
+            }
         })
-        res.json(units)
     })
+    res.json(units)
 })
 
 
